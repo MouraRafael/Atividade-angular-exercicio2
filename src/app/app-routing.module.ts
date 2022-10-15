3eb3484b75c446ebd6527535aace1964304f7211
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-15 20:17:52 -0300

    feat: criacao de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JogoComponent } from './atividade/component/jogo/jogo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'atividade/jogo',component:JogoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
