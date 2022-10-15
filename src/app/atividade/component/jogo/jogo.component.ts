25c19c8f2b2d916692811a800bc98787005a756b
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-15 20:28:32 -0300

    feat: validacao do num aleatorio

--- src/app/atividade/component/jogo/jogo.component.ts
+++ src/app/atividade/component/jogo/jogo.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jogo.component.css']
 })
 export class JogoComponent implements OnInit {
-
+  baseAleaatoria: number = this.geraAleatorio();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  geraAleatorio():number{
+      var aleatorio = Math.floor(Math.random()*100);
+
+      return (aleatorio == 0) ?  this.geraAleatorio() : aleatorio;
+
   }
 
 }
