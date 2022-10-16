3ebaae4d03765b2e8c5004098d012e80ee30ab76
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-16 08:25:45 -0300

    feat:fnComparacao inicial + validacao

--- src/app/atividade/component/jogo/jogo.component.ts
+++ src/app/atividade/component/jogo/jogo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { JogoModel } from '../model/jogo.model';
 
 @Component({
   selector: 'app-jogo',
@@ -7,15 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoComponent implements OnInit {
   baseAleatoria: number = this.geraAleatorio();
-  constructor() { }
+
+  formAdvinha!:FormGroup;
+
+  resultado!: string;
+
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+      this.formAdvinha = this.formBuilder.group({
+      entrada: ['',[Validators.required,Validators.pattern(/^[0-9]?[0-9]$|^100$/)]],
+    })
   }
 
   geraAleatorio():number{
       return Math.floor(Math.random()*100) + 1;
   }
 
+  comparaNumeros(){
+    const calcula = this.formAdvinha.getRawValue() as JogoModel;
 
+    this.resultado = (this.baseAleatoria == calcula.entrada) ? 'Você Acertou! Parabéns':'Você Errou! Tente Novamente!'
+
+
+  }
+
+  get entrada() {return this.formAdvinha.get('entrada')!}
 
 }
