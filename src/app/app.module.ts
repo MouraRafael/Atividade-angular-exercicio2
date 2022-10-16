52aa4f84cd31c2dda4df80bb34cc3f50913e6965
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-16 08:26:08 -0300

    feat: import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +16,7 @@ import { JogoComponent } from './atividade/component/jogo/jogo.component';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
